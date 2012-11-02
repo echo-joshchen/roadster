3af85301475f0eded91830728cf2655c0776c7e4
John Du <johnldu@gmail.com>
2012-11-01 22:50:22 -0700

    Implimented basic search

--- scripts/map.js
+++ scripts/map.js
@@ -4,6 +4,8 @@ var MBAcquarium = [36.618051,-121.902061,"Monterey Bay Aquarium"];
 var PismoBeach = [35.086115,-120.622912,"Pismo Beach"];
 var Disneyland = [33.809391,-117.918924,"Disneyland"];
 var pointsToAdd = [MBAcquarium, PismoBeach, Disneyland];
+var pointsToSearch = [MBAcquarium, PismoBeach, Disneyland];
+var searchNearValues = [MBAcquarium[2], PismoBeach[2], Disneyland[2]];
 var map;
 var path = [];
 
@@ -86,6 +88,33 @@ function updatePath() {
 	path = newPath;
 }
 
+// Adjusts map and sidebar to respond to a search.
+function search(coord, value) {
+	map.addLayer('places', {
+		location: new google.maps.LatLng(coord[0], coord[1]),
+		radius: 10000,
+		keyword: value,
+		search: function (results, status) {
+			if (status == google.maps.places.PlacesServiceStatus.OK) {
+				for (var i = 0; i < results.length; i++) {
+					var place = results[i];
+					map.addMarker({
+						lat: place.geometry.location.lat(),
+						lng: place.geometry.location.lng(),
+						title: place.name,
+						size: 'small',
+						infoWindow: {
+							content: '<h2>'+place.name+'</h2>'
+						}
+					});
+				}
+			}
+		}
+	});
+	map.setCenter(coord[0], coord[1]);
+	map.setZoom(15);
+}
+
 $(document).ready(function(){
 
 	createMap();
@@ -99,7 +128,9 @@ $(document).ready(function(){
 			addPoint(pointsToAdd.shift());
 		} else if (action == "search") {
 			// IMPLEMENT SEARCH FUNCTIONALITY
-			alert('Search coming soon');
+			$("#searchbar").show();
+			$("#itinerary").hide();
+			document.getElementById("near").value = searchNearValues.shift();
 		}
 	});
 
@@ -114,4 +145,9 @@ $(document).ready(function(){
 		placeHolderTemplate: ""
 	});
 
+	$("#searchbar").hide();
+
+	$("#submit").click(function() {
+		search(pointsToSearch.shift(), document.getElementById("keyword").value);
+	});
 });
